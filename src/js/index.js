805804b23f8ca7ed5c3bd939963c66b242c448de
Luis <166702635+LuisIannelloDev@users.noreply.github.com>
2024-06-21 22:16:55 +0000

    Landing Page - Luis

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,16 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import { appName, logoUrl } from "./component/appName.js";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import Jumbotron from "./component/Jumbotron.jsx";
+import Cards from "./component/cards.jsx";
+
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Home appName= {appName}  logoUrl={logoUrl}/>, document.querySelector("#app"));
